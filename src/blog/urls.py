f0e95ce10fbb44dd0c445df6c4b571756887fa39
Pedro Henrique <pedro.schleder@gmail.com>
2020-07-02 16:18:40 -0300

    Adding login/logout feature

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 from accounts.views import UserRegistrationView
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('new-user/', UserRegistrationView.as_view(), name='user_registration'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
 ]
